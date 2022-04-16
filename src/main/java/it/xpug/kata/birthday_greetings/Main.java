a5abde7d424da04bb342f25cf67ed65bf6dcebac
ElHadji Omar DIONE <omardione1991@gmail.com>
2022-04-16 01:03:29 +0000

    cleaning

--- src/main/java/it/xpug/kata/birthday_greetings/Main.java
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.java
@@ -8,24 +8,20 @@ import it.xpug.kata.birthday_greetings.infrastructure.EmailServiceAdapter;
 import it.xpug.kata.birthday_greetings.infrastructure.FlatFileEmployeeAdapter;
 import it.xpug.kata.birthday_greetings.infrastructure.MailProperties;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Main {
 
-	private static final Logger logger = Logger.getLogger("Main");
-
-	// Classes du domaine métier
-	// Classes de configuration
 	public static void main(String[] args) {
-		logger.log(Level.INFO, "Run...");
 		EmployeeRepository employeeRepository = new FlatFileEmployeeAdapter("employee_data.txt");
 		MailProperties mailProperties = new MailProperties("sender@here.com", "localhost", 25);
 		NotificationProvider notificationProvider = new EmailServiceAdapter(mailProperties);
 
 		BirthdayService service = new BirthdayService(employeeRepository, notificationProvider);
 
-		service.sendGreetings(new XDate());
+		service.sendGreetings(today());
+	}
+
+	private static XDate today() {
+		return new XDate();
 	}
 
 }
